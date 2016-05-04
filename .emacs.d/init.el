a5fecb8969101f55bf872ceb628c4ec9ac1d1991
Matt Hughes <mahughes@wayfair.com>
2016-05-04 09:29:05 -0400

    Updated evil search option to be the more vimmy

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -71,7 +71,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-search-module (quote evil-search)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
