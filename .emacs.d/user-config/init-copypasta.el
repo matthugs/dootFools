31914c67a2a4a855fbc84f0f6eb9dbacc6aa052e
Matt Hughes <6405097+matthugs@users.noreply.github.com>
2025-08-21 17:24:46 -0400

    feat(emacs): add command for getting source control links
    i.e. if you're visiting a file in a repo cloned from a gitlab or a
    github origin, get-source-control-link will kill (which in my config
    also copies to the system clipboard) a stable link to the location in
    code at point.
    
    (I say "stable" because I find it frustrating when links to specific
    lines stop working as a branch changes.)

--- .emacs.d/user-config/init-copypasta.el
+++ .emacs.d/user-config/init-copypasta.el
@@ -32,4 +32,36 @@
          (setq interprogram-paste-function 'copy-from-osx)
          )))
 
+
+(defun parse-repo-path-from-url (url)
+  "given a remote url for git repos return the repository path"
+  (let* ((parse-attempt (url-generic-parse-url url))
+         (is-compliant-url? (url-host parse-attempt)))
+    (if is-compliant-url?
+        (s-chop-suffix ".git" (url-filename parse-attempt))
+      ;; assume we're dealing with a git-style-url
+      ;; chop at the colon
+      (s-chop-suffix ".git" (cadr (s-split ":" url)))
+      )
+    )
+  )
+
+; TODO: explore just opening the tab from emacs
+(defun get-source-control-link ()
+  "guess a source control link based on the current buffer's projectile root"
+  (interactive)
+  (let* ((remote-url (magit-get "remote" (magit-primary-remote) "url"))
+         (is-gitlab? (s-contains? "gitlab.com" remote-url))
+         (is-github? (s-contains? "github.com" remote-url))
+         (commit (magit-commit-p (magit-headish)))
+         (path-to-visited-file (s-chop-prefix (projectile-project-root) buffer-file-name))
+         (line-number-of-point (number-to-string (line-number-at-pos)))
+         (repo-path (parse-repo-path-from-url remote-url)))
+    (if is-gitlab?
+        (kill-new (concat "https://gitlab.com" repo-path "-/tree/" commit "/" path-to-visited-file "#L" line-number-of-point))
+      (if is-github?
+          (kill-new (concat "https://github.com/" repo-path "/blob/" commit "/" path-to-visited-file "#L" line-number-of-point))
+  ))))
+
+
 (provide 'init-copypasta)
