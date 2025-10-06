cd1453eec5b4d27b5ac32827b2a82242711fd313
Matt Hughes <6405097+matthugs@users.noreply.github.com>
2025-10-06 15:14:54 -0400

    feat(firefox): another ai opt-out option

--- snap/firefox/common/.mozilla/firefox/name-of-profile-which-may-be-random/user.js
+++ snap/firefox/common/.mozilla/firefox/name-of-profile-which-may-be-random/user.js
@@ -15,6 +15,7 @@ user_pref("browser.ml.chat.enabled", false);
 user_pref("browser.ml.chat.shortcuts", false);
 user_pref("browser.ml.chat.shortcuts.custom", false);
 user_pref("browser.ml.chat.sidebar", false);
+user_pref("browser.ml.chat.menu", false);
 user_pref("extensions.ml.enable", false);
 user_pref("browser.tabs.groups.smart.enabled", false);
 user_pref("browser.tabs.groups.smart.optin", false);
