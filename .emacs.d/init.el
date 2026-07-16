248e86da297f03b590c7f8e649e5dda5228f0b7b
Matt Hughes <6405097+matthugs@users.noreply.github.com>
2026-07-16 18:30:10 -0400

    feat(emacs): set up eglot on python buffers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,7 +25,7 @@
 (require 'init-php)
 (require 'init-scala)
 (require 'init-terraform)
-(require 'init-rust)
+;; (require 'init-rust)
 (require 'init-yaml)
 (require 'init-treesit)
 (require 'init-copypasta)
--- .emacs.d/user-config/init-python.el
+++ .emacs.d/user-config/init-python.el
@@ -5,4 +5,8 @@
 (setq major-mode-remap-alist
       '((python-mode . python-ts-mode)))
 
+(use-package eglot
+  :ensure nil
+  :hook ((python-ts-mode . eglot-ensure)))
+
 (provide 'init-python)
