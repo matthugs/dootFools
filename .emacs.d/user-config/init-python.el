c687278f9dd089ea3505051381faf86ed8f9bf5a
Matt Hughes <6405097+matthugs@users.noreply.github.com>
2026-07-16 17:23:34 -0400

    feat(emacs): switch to using python treesitter mode by default
    learned about `major-mode-remap-alist` from
    https://gist.github.com/habamax/290cda0e0cdc6118eb9a06121b9bc0d7

--- .emacs.d/user-config/init-python.el
+++ .emacs.d/user-config/init-python.el
@@ -1,4 +1,8 @@
+(require 'init-treesit)
 
 (add-to-list 'auto-mode-alist '("/\\.coveragerc\\'" . conf-mode))
+
+(setq major-mode-remap-alist
+      '((python-mode . python-ts-mode)))
 
 (provide 'init-python)
--- .emacs.d/user-config/init-treesit.el
+++ .emacs.d/user-config/init-treesit.el
@@ -2,7 +2,8 @@
 (setq treesit-language-source-alist
       '(
         (yaml "https://github.com/tree-sitter-grammars/tree-sitter-yaml.git" "v0.7.0")
-        (json "https://github.com/tree-sitter/tree-sitter-json.git" "v0.24.8")))
+        (json "https://github.com/tree-sitter/tree-sitter-json.git" "v0.24.8")
+        (python "https://github.com/tree-sitter/tree-sitter-python" "v.0.25.0")))
 
 (use-package json-ts-mode
   :mode "\\.json\\'")
